3a8e8f47ffb93ad34aa5b6a34f982da42b3efb9f
OrvokkiK <orvokki.kettula@metropolia.fi>
2024-01-22 16:51:13 +0200

    Added missing type to package.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 // Main JS file
-console.log('Kato moro!');
+// index.js
+import http from 'http';
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// import http from 'http';
-// const hostname = '127.0.0.1';
-// const port = 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome to my REST API!');
+});
 
-// const server = http.createServer((req, res) => {
-//  res.writeHead(200, { 'Content-Type': 'text/plain' });
-//  res.end('Welcome to my REST API!');
-// });
-
-// server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
